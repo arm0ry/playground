89de6a3be39d10522d4b942204dcad86b12cd51c
audsssy <stamfordh@hotmail.com>
2023-07-16 15:09:44 -0700

    update interface

---
+++ src/interface/IKaliTokenManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/// @notice Kali DAO share manager interface
+interface IKaliTokenManager {
+    function mintShares(address to, uint256 amount) external payable;
+
+    function burnShares(address from, uint256 amount) external payable;
+
+    function balanceOf(address account) external view returns (uint256);
+
+}
--- src/interface/IMissions.sol
+++ src/interface/IMissions.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.17;
 
 interface IMissions {
-    function isTaskInMission(uint256 missionId, uint256 taskId)
-        external
-        returns (bool);
+    function isTaskInMission(uint256 missionId, uint256 taskId) external returns (bool);
 
     function getTask(uint256 taskId) external view returns (uint8, uint40, address, string memory, string memory);
 
-    function getMission(uint256 _missionId) external view returns (uint8, uint40, uint8[] memory, string memory, string memory, address, uint8, uint256, uint256);
+    function getMission(uint256 _missionId)
+        external
+        view
+        returns (uint8, uint40, uint8[] memory, string memory, string memory, address, uint256, uint256, uint256);
 }
