f2b433f6fd2ac8e961d4ec74d12dace03cb28ea6
audsssy <stamfordh@hotmail.com>
2023-07-16 15:07:24 -0700

    clean up

--- src/interface/IKaliShareManager.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-/// @notice Kali DAO share manager interface
-interface IKaliShareManager {
-    function mintShares(address to, uint256 amount) external payable;
-
-    function burnShares(address from, uint256 amount) external payable;
-}
