7d7f60b473ab7ee4588749ca1f2ea22ba45df53b
audsssy <stamfordh@hotmail.com>
2023-08-11 15:14:00 -0700

    🧑‍🍳 add QuestsDirectory for access list & stats

---
+++ src/utils/NTERC1155.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.4;
+
+/// @notice Non-transferable multi-token with total supplies based on ERC-1155
+/// @author Modified from Kail (https://github.com/kalidao/kali-contracts/blob/main/contracts/tokens/erc1155/NTERC1155.sol)
+/// License-Identifier AGPL-3.0-only
+abstract contract NTERC1155 {
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event TransferSingle(
+        address indexed operator, address indexed from, address indexed to, uint256 id, uint256 amount
+    );
+
+    event URI(string value, uint256 indexed id);
+
+    /*//////////////////////////////////////////////////////////////
+                             ERC1155 STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    mapping(address => mapping(uint256 => uint256)) public balanceOf;
+
+    mapping(uint256 => uint256) public _totalSupply;
+
+    /*//////////////////////////////////////////////////////////////
+                             METADATA LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function uri(uint256 id) public view virtual returns (string memory);
+
+    /*//////////////////////////////////////////////////////////////
+                              ERC1155 LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function balanceOfBatch(address[] calldata owners, uint256[] calldata ids)
+        public
+        view
+        virtual
+        returns (uint256[] memory balances)
+    {
+        require(owners.length == ids.length, "LENGTH_MISMATCH");
+
+        balances = new uint256[](owners.length);
+
+        // Unchecked because the only math done is incrementing
+        // the array index counter which cannot possibly overflow.
+        unchecked {
+            for (uint256 i = 0; i < owners.length; ++i) {
+                balances[i] = balanceOf[owners[i]][ids[i]];
+            }
+        }
+    }
+
+    function totalSupply(uint256 id) public view virtual returns (uint256) {
+        return _totalSupply[id];
+    }
+
+    function exists(uint256 id) public view virtual returns (bool) {
+        return totalSupply(id) > 0;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              ERC165 LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return interfaceId == 0x01ffc9a7 // ERC-165 Interface ID for ERC-165
+            || interfaceId == 0xd9b67a26 // ERC-165 Interface ID for ERC-1155
+            || interfaceId == 0x0e89341c; // ERC-165 Interface ID for ERC1155MetadataURI
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        INTERNAL MINT/BURN LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _mint(address to, uint256 id, uint256 amount, bytes memory data) internal virtual {
+        balanceOf[to][id] += amount;
+        ++_totalSupply[id];
+
+        emit TransferSingle(msg.sender, address(0), to, id, amount);
+
+        require(
+            to.code.length == 0
+                ? to != address(0)
+                : ERC1155TokenReceiver(to).onERC1155Received(msg.sender, address(0), id, amount, data)
+                    == ERC1155TokenReceiver.onERC1155Received.selector,
+            "UNSAFE_RECIPIENT"
+        );
+    }
+
+    function _burn(address from, uint256 id, uint256 amount) internal virtual {
+        balanceOf[from][id] -= amount;
+        --_totalSupply[id];
+
+        emit TransferSingle(msg.sender, from, address(0), id, amount);
+    }
+}
+
+/// @notice A generic interface for a contract which properly accepts ERC-1155 tokens
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC1155.sol)
+/// License-Identifier AGPL-3.0-only
+abstract contract ERC1155TokenReceiver {
+    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external virtual returns (bytes4) {
+        return ERC1155TokenReceiver.onERC1155Received.selector;
+    }
+}
