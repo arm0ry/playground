9bac01a7c559de17971b33f21e5da23e9596e748
audsssy <stamfordh@hotmail.com>
2023-08-14 15:16:43 -0700

    🥢 nits

--- src/interface/IQuests.sol
+++ src/interface/IQuests.sol
@@ -5,4 +5,14 @@ interface IQuests {
     function getMissionCompletionsCount(uint8 missionId) external view returns (uint256);
 
     function mission() external view returns (address);
+
+    function encode(address tokenAddress, uint256 tokenId, address missions, uint256 missionId, uint256 taskId)
+        external
+        pure
+        returns (bytes memory);
+
+    function decode(bytes calldata b)
+        external
+        pure
+        returns (address tokenAddress, uint256 tokenId, address missions, uint256 missionId, uint256 taskId);
 }
