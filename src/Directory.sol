a2fded46a9f0dfdb0f9bd77b921e46f2db0b4931
audsssy <stamfordh@hotmail.com>
2023-08-18 16:28:22 -0700

    Update Directory.sol
    - add access list storage and logic
    - add SafeMulticallable

--- src/Directory.sol
+++ src/Directory.sol
@@ -1,34 +1,24 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
+import {SafeMulticallable} from "solbase/utils/SafeMulticallable.sol";
+
 /// @notice Directory for Quests
 /// @author Modified from Kali (https://github.com/kalidao/kali-contracts/blob/main/contracts/access/KaliAccessManager.sol)
 /// @author Storage pattern inspired by RocketPool (https://github.com/rocket-pool/rocketpool/blob/6a9dbfd85772900bb192aabeb0c9b8d9f6e019d1/contracts/contract/RocketStorage.sol)
 
-abstract contract Directory {
+contract Directory is SafeMulticallable {
     /// -----------------------------------------------------------------------
     /// Errors
     /// -----------------------------------------------------------------------
 
     error NotOperator();
-    error ListClaimed();
-    error NotListed();
-    error InvalidMission();
+
+    error LengthMismatch();
 
     /// -----------------------------------------------------------------------
     /// List Storage
     /// -----------------------------------------------------------------------
-
-    // Storage Keys (API into quest stats)
-    //
-    // Keys:
-    // - quest.{key}.{questActivity}
-    //
-    // Example:
-    // - quest.{questKey}.start -> uintStorage
-    //      - missions.{missionId}.startCount -> uintStorage
-    // - quest.{taskKey}.response -> stringStorage
-    // - quest.{taskKey}.review -> boolStorage
 
     address public dao;
     mapping(bytes32 => string) public stringStorage;
@@ -44,7 +34,7 @@ abstract contract Directory {
     }
 
     /// -----------------------------------------------------------------------
-    /// Setter Logic
+    /// General Storage - Setter Logic
     /// -----------------------------------------------------------------------
 
     /// @param newDao The address of new DAO
@@ -73,7 +63,7 @@ abstract contract Directory {
     }
 
     /// -----------------------------------------------------------------------
-    /// Delete Logic
+    /// General Sotrage - Delete Logic
     /// -----------------------------------------------------------------------
 
     /// @param _key The key for the record
@@ -113,36 +103,137 @@ abstract contract Directory {
     }
 
     /// -----------------------------------------------------------------------
-    /// Getter Logic
+    /// General Storage - Getter Logic
     /// -----------------------------------------------------------------------
 
-    /// @dev Get the address of a network contract by name
-    function getContractAddress(string memory _contractName) internal view returns (address) {
-        // Get the current contract address
-        address contractAddress = this.getAddress(keccak256(abi.encodePacked("contract.address", _contractName)));
-        // Check it
-        require(contractAddress != address(0x0), "Contract not found");
-        // Return
-        return contractAddress;
+    /// @dev Get the address of DAO.
+    function getDao() external view returns (address) {
+        return dao;
     }
 
-    /// @param _key The key for the record
+    /// @dev Get the address of quests contract.
+    function getQuestsAddress() external view returns (address) {
+        return this.getAddress(keccak256(abi.encodePacked("quests")));
+    }
+
+    /// @param _key The key for the record.
     function getAddress(bytes32 _key) external view returns (address r) {
         return addressStorage[_key];
     }
 
-    /// @param _key The key for the record
+    /// @param _key The key for the record.
     function getUint(bytes32 _key) external view returns (uint256 r) {
         return uintStorage[_key];
     }
 
-    /// @param _key The key for the record
+    /// @param _key The key for the record.
     function getString(bytes32 _key) external view returns (string memory) {
         return stringStorage[_key];
     }
 
-    /// @param _key The key for the record
+    /// @param _key The key for the record.
     function getBool(bytes32 _key) external view returns (bool r) {
         return booleanStorage[_key];
+    }
+
+    /// -----------------------------------------------------------------------
+    /// AccessList Storage - Setter Logic
+    /// -----------------------------------------------------------------------
+
+    function setAccessList(uint256 id, address[] calldata accounts, bool[] calldata approvals)
+        external
+        onlyPlaygroundOperators
+    {
+        uint256 length = accounts.length;
+        if (length != approvals.length) revert LengthMismatch();
+
+        // Update old list.
+        if (id != 0) {
+            // Increment and store total number of accounts on a list.
+            uint256 accountCount = this.getUint(keccak256(abi.encodePacked("access.", id, ".count")));
+
+            for (uint256 i; i < accountCount;) {
+                address _account = this.getAddress(keccak256(abi.encodePacked("access.", id, i)));
+                if (_account == accounts[i]) {
+                    // Store approval
+                    this.setBool(keccak256(abi.encodePacked("access.", id, accounts[i])), approvals[i]);
+                }
+
+                unchecked {
+                    ++i;
+                }
+            }
+        } else {
+            // Increment and store total number of access lists.
+            uint256 listCount = this.getUint(keccak256(abi.encodePacked("accessListCount")));
+            this.setUint(keccak256(abi.encodePacked("accessListCount")), ++listCount);
+
+            uint256 accountCount;
+
+            // Increment and store total number of accounts on list.
+            for (uint256 i; i < length;) {
+                // Set account
+                this.setAddress(keccak256(abi.encodePacked("access.", listCount, accountCount)), accounts[i]);
+                // Store approval
+                this.setBool(keccak256(abi.encodePacked("access.", id, accounts[i])), approvals[i]);
+
+                unchecked {
+                    ++i;
+                    ++accountCount;
+                }
+            }
+
+            this.setUint(keccak256(abi.encodePacked("access.", listCount, ".count")), accountCount);
+        }
+    }
+
+    /// -----------------------------------------------------------------------
+    /// AccessList Storage - Delete Logic
+    /// -----------------------------------------------------------------------
+
+    function deleteAccessList(uint256 id) external onlyPlaygroundOperators {
+        // Increment and store total number of accounts on a list.
+        uint256 accountCount = this.getUint(keccak256(abi.encodePacked("access.", id, ".count")));
+
+        for (uint256 i; i < accountCount;) {
+            address _account = this.getAddress(keccak256(abi.encodePacked("access.", id, i)));
+
+            // Delete account.
+            this.deleteBool(keccak256(abi.encodePacked("access.", id, i)));
+
+            // Delete approval.
+            this.deleteBool(keccak256(abi.encodePacked("access.", id, _account)));
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// -----------------------------------------------------------------------
+    /// AccessList Storage - Getter Logic
+    /// -----------------------------------------------------------------------
+
+    function getAccessList(uint256 id) external view returns (address[] memory) {
+        // Increment and store total number of accounts on a list.
+        uint256 accountCount = this.getUint(keccak256(abi.encodePacked("access.", id, ".count")));
+        address[] memory list = new address[](accountCount);
+
+        for (uint256 i; i < accountCount;) {
+            // Retrieve addresses approval.
+            address account = this.getAddress(keccak256(abi.encodePacked("access.", id, i)));
+
+            list[i] = account;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        return list;
+    }
+
+    function getAccessListStatus(uint256 id, address account) external view returns (bool) {
+        return this.getBool(keccak256(abi.encodePacked("access.", id, account)));
     }
 }
