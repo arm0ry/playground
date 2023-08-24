e3a64f35a909b9eb7ff951960952f1c6217bd778
audsssy <stamfordh@hotmail.com>
2023-08-23 17:17:58 -0700

    Update Travelers.sol

--- src/Travelers.sol
+++ src/Travelers.sol
@@ -1,5 +1,5 @@
 // // SPDX-License-Identifier: AGPL-3.0-only
-// pragma solidity >=0.8.4;
+pragma solidity >=0.8.4;
 
 // import {ERC721} from "solbase/tokens/ERC721/ERC721.sol";
 // import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
