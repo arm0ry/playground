7aa91e2920321944e4d72c151fb2197e5e0a5890
audsssy <stamfordh@hotmail.com>
2023-09-08 13:27:09 -0700

    clean up

--- src/interface/IQuest.sol
+++ src/interface/IQuest.sol
@@ -1,18 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-interface IQuest {
-    function getMissionCompletionsCount(uint8 missionId) external view returns (uint256);
+import {QuestDetail} from "../Quest.sol";
 
-    function mission() external view returns (address);
+interface IQuest {
+    function getQuestDetail(bytes32 questKey) external view returns (QuestDetail memory);
 
     function encode(address tokenAddress, uint256 tokenId, address missions, uint256 missionId, uint256 taskId)
         external
         pure
         returns (bytes memory);
-
-    function decode(bytes calldata b)
-        external
-        pure
-        returns (address tokenAddress, uint256 tokenId, address missions, uint256 missionId, uint256 taskId);
 }
