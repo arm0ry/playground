f4430bca6ce2c5484d7dd84ecba633023628dd56
audsssy <stamfordh@hotmail.com>
2023-11-01 16:10:51 -0700

    Delete IImpactToken.sol

--- src/interface/IImpactToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IImpactToken {
-    function mint(address to, uint256 id) external;
-    function getTokenId(address missions, uint256 missionId) external pure returns (uint256);
-    function decodeTokenId(uint256 tokenId) external pure returns (address missions, uint256 missionId);
-}
