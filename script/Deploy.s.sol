a6b95c977ed3119a6378189b4877929f83f3280e
audsssy <stamfordh@hotmail.com>
2023-12-15 13:09:16 -0800

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,22 +4,28 @@ pragma solidity ^0.8.17;
 import {Script} from "forge-std/Script.sol";
 import "forge-std/console.sol";
 
-import {Quest} from "src/Quest.sol";
-import {QuestFactory} from "src/QuestFactory.sol";
 import {Mission} from "src/Mission.sol";
-import {MissionFactory} from "src/MissionFactory.sol";
-import {TokenFactory} from "src/TokenFactory.sol";
+import {Quest} from "src/Quest.sol";
+import {Factory} from "src/Factory.sol";
+import {ImpactCurve} from "src/ImpactCurve.sol";
 
+import {IImpactCurve, CurveType} from "../src/interface/IImpactCurve.sol";
 import {MissionBergerToken} from "src/tokens/MissionBergerToken.sol";
 import {MissionSupportToken} from "src/tokens/MissionSupportToken.sol";
-import {QuestSupportToken} from "src/tokens/QuestSupportToken.sol";
+import {qSupportToken} from "src/tokens/qSupportToken.sol";
+import {mSupportToken} from "src/tokens/mSupportToken.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
-    QuestFactory qFactory;
-    address mContract;
-    MissionFactory mFactory;
-    TokenFactory tFactory;
+    // Contracts.
+    Mission mContract;
+    Quest qContract;
+    Factory fContract;
+    ImpactCurve icContract;
+
+    // Tokens.
+    mSupportToken mstContract;
+    qSupportToken qstContract;
 
     // Prep for task submission.
     address[] taskCreators;
@@ -36,64 +42,56 @@ contract Deploy is Script {
 
         console.log("Account", account);
 
+        address user = address(0x4744cda32bE7b3e75b9334001da9ED21789d4c0d);
+
         vm.startBroadcast(privateKey);
 
-        // deployQuestFactory();
-        deployMissionFactory(account);
-        deployTokenFactory();
-        mintMissionBergerToken(account);
-        // deployMissionSupportTokenFactory();
-        // deployQuestSupportTokenFactory();
+        deployFactory(user);
+        deployImpactCurve(user);
 
         vm.stopBroadcast();
     }
 
-    function deployQuestFactory() internal {
-        Quest template = new Quest();
-        qFactory = new QuestFactory(address(template));
+    function deployFactory(address user) internal {
+        // Templates.
+        Quest qTemplate = new Quest();
+        Mission mTemplate = new Mission();
+        mSupportToken mstTemplate = new mSupportToken();
+        qSupportToken qstTemplate = new qSupportToken();
+
+        fContract = new Factory(address(mTemplate), address(mstTemplate), address(qTemplate), address(qstTemplate));
     }
 
-    function deployMissionFactory(address user) internal {
-        Mission template = new Mission();
-        mFactory = new MissionFactory(address(template));
-
-        // Add first task.
-        taskCreators.push(user);
-        taskDeadlines.push(100000000);
-        taskDetail.push("FIRST TASK");
-
-        // Add second task.
-        taskCreators.push(user);
-        taskDeadlines.push(100000000000000000);
-        taskDetail.push("SECOND TASK");
-
-        // Submit tasks onchain.
-        mContract = mFactory.deployMission(user);
-        Mission(mContract).initialize(user);
-        Mission(mContract).setTasks(taskCreators, taskDeadlines, taskDetail);
-
-        taskIds.push(1);
-        taskIds.push(2);
-
-        // Submit mission onchain.
-        Mission(mContract).setMission(user, "FIRST MISSION EVER", "IT ALL BEGINS..", taskIds);
+    function deployImpactCurve(address user) internal {
+        icContract = new ImpactCurve();
     }
 
-    function deployTokenFactory() internal {
-        tFactory = new TokenFactory();
-    }
+    // function deployMissionFactory(address user) internal {
+    //     Mission template = new Mission();
 
-    function mintMissionBergerToken(address user) internal {
-        MissionBergerToken mbt_template = new MissionBergerToken();
-        tFactory.addToken(address(mbt_template), "Mission Harberger Token");
-        address token = tFactory.deploy(tFactory.count());
-        console.log("Token Address", token);
+    //     // Add first task.
+    //     taskCreators.push(user);
+    //     taskDeadlines.push(100000000);
+    //     taskDetail.push("FIRST TASK");
 
-        MissionBergerToken(token).initialize(user, user);
-        MissionBergerToken(token).mint(mContract, 1);
-    }
+    //     // Add second task.
+    //     taskCreators.push(user);
+    //     taskDeadlines.push(100000000000000000);
+    //     taskDetail.push("SECOND TASK");
 
-    function deployMissionSupportTokenFactory() internal returns (MissionSupportToken) {}
+    //     // Deploy mission contract.
+    //     mContract = mFactory.deployMission(user);
+    //     Mission(mContract).initialize(user);
 
-    function deployQuestSupportTokenFactory() internal returns (QuestSupportToken) {}
+    //     // Submit tasks onchain.
+    //     Mission(mContract).setTasks(taskCreators, taskDeadlines, taskDetail);
+
+    //     taskIds.push(1);
+    //     taskIds.push(2);
+
+    //     // Submit mission onchain.
+    //     Mission(mContract).setMission(
+    //         user, unicode"g0v 第伍拾玖次輪班寫 code 救台灣黑客松", "IT ALL BEGINS..", taskIds
+    //     );
+    // }
 }
