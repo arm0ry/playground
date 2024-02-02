1eb5ad1795f54c9e35dd20dca3e9ff0b192524c0
audsssy <stamfordh@hotmail.com>
2024-02-02 15:10:14 -0800

    🛝 deploy a playground

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,24 +10,30 @@ import {Factory} from "src/Factory.sol";
 import {ImpactCurve} from "src/ImpactCurve.sol";
 
 import {IImpactCurve, CurveType} from "../src/interface/IImpactCurve.sol";
-import {qSupportToken} from "src/tokens/qSupportToken.sol";
-import {mSupportToken} from "src/tokens/mSupportToken.sol";
+import {OnboardingSupportToken} from "src/tokens/g0v/OnboardingSupportToken.sol";
+import {HackathonSupportToken} from "src/tokens/g0v/HackathonSupportToken.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
+    error Invalid();
+
+    // Constant.
+    uint256 constant future = 2527482181;
+
     // Contracts.
     address mContract;
     address qContract;
     address fContract;
-    address payable icContract;
+    address payable icContract = payable(address(0x80179A82472F950e3ae5235D7934E5A5BC161170));
 
     // Tokens.
-    address mstContract;
-    address qstContract;
+    address hackathonContract;
+    address onboardingContract;
 
     // Prep for task submission.
     address[] taskCreators;
     uint256[] taskDeadlines;
+    string[] taskTitles;
     string[] taskDetail;
 
     // Prep for mission submission.
@@ -45,31 +51,51 @@ contract Deploy is Script {
 
         vm.startBroadcast(privateKey);
 
-        deployFactory(account, user1, user2);
-        // deployImpactCurve(user);
+        deployG0vPlayground(account, user1, user2);
+        // deployOnboardingSupportToken(user1, mContract, 1, qContract, icContract);
+        // deployFoodList(user2);
+        // deployMeditationJourney(user1);
 
         vm.stopBroadcast();
     }
 
-    function deployFactory(address patron, address user, address user2) internal {
+    function deployG0vPlayground(address patron, address user, address user2) internal {
         // Templates for factory deployment.
         Quest qTemplate = new Quest();
         Mission mTemplate = new Mission();
-        mSupportToken mstTemplate = new mSupportToken();
 
         // 1. Deploy factory.
-        fContract = address(new Factory(address(mTemplate), address(mstTemplate), address(qTemplate), address(0)));
+        Factory factory = new Factory(address(mTemplate),  address(qTemplate));
+        fContract = address(factory);
 
-        // 1. Deploy curves.
+        // 2. Deploy quest contract and complete tasks.
+        deployQuest(user);
+
+        // 3. Deploy curves.
         deployImpactCurve(user);
 
-        // 2. Deploy and prepare mission.
+        // 4. Deploy mission contract.
         deployMission(patron);
-        setTasksAndMission(user, user2);
+        Mission(mContract).setPriceCurve(icContract, 1);
 
-        // 2. Deploy quest and complete tasks.
-        deployQuest(user);
+        // 5. Deploy support token.
+        deployHackathonSupportToken(mContract, qContract, icContract);
+        HackathonSupportToken(hackathonContract).setSvgInputs(1, 5);
+
+        // 6. Set curve.
+        ImpactCurve(icContract).curve(CurveType.LINEAR, hackathonContract, user, 0.0001 ether, 0, 10, 0, 0, 5, 0);
+
+        // 7. Prepare hackathon.
+        deployHackath0n(user, user2);
+        deployHackath0nLunch(user);
+
+        // 8. Authorize quest contract to record stats in mission contract.
         Mission(mContract).authorizeQuest(qContract, true);
+
+        // 9. Update admin.
+        Mission(mContract).setDao(user);
+
+        // 10. Submit mock user input.
         Quest(qContract).start(address(mContract), 1);
         Quest(qContract).respond(address(mContract), 1, 1, 1, "First Task Done!");
         Quest(qContract).respond(address(mContract), 1, 1, 1, "First Task Done!");
@@ -82,24 +108,15 @@ contract Deploy is Script {
         Quest(qContract).respond(address(mContract), 1, 2, 2, "Second Task Done!");
         Quest(qContract).respond(address(mContract), 1, 3, 2, "Third Task Done!");
         Quest(qContract).respond(address(mContract), 1, 3, 2, "Third Task Done!");
-        Quest(qContract).respond(address(mContract), 1, 4, 2, "Third Task Done!");
+        Quest(qContract).respond(address(mContract), 1, 4, 2, "Fourth Task Done!");
+        Quest(qContract).respond(address(mContract), 1, 4, 2, "Fourth Task Done!");
+        Quest(qContract).respond(address(mContract), 1, 4, 2, "Fourth Task Done!");
+        Quest(qContract).respond(address(mContract), 1, 4, 2, "Fourth Task Done!");
+        Quest(qContract).respond(address(mContract), 1, 5, 2, "Fifth Task Done!");
 
-        // 2. Deploy support token.
-        mstContract = Factory(fContract).deploySupportToken(
-            "g0v Jothon Support Token", "g0vST", user, address(0), address(mContract), 1, address(icContract)
-        );
-
-        // 3. Set curve.
-        ImpactCurve(icContract).curve(CurveType.LINEAR, mstContract, user, 0.0001 ether, 0, 10, 0, 0, 1, 0);
-
-        // 4. Patron support.
-        uint256 price = ImpactCurve(icContract).getPrice(true, 1, 0);
+        // 11. Submit mock patron support.
+        uint256 price = ImpactCurve(icContract).getCurvePrice(true, 1, 0);
         ImpactCurve(icContract).support{value: price}(1, patron, price);
-    }
-
-    function deployImpactCurve(address user) internal {
-        icContract = payable(address(new ImpactCurve()));
-        ImpactCurve(icContract).initialize(user);
     }
 
     function deployMission(address user) internal {
@@ -112,41 +129,275 @@ contract Deploy is Script {
         Quest(qContract).initialize(user);
     }
 
-    function setTasksAndMission(address user, address user2) internal {
+    function deployImpactCurve(address user) internal {
+        icContract = payable(address(new ImpactCurve()));
+        ImpactCurve(icContract).initialize(user);
+    }
+
+    function deployHackathonSupportToken(address _mContract, address _qContract, address _icContract) internal {
+        HackathonSupportToken supportToken =
+            new HackathonSupportToken("g0v Hackathon Support Token", "g0vHST", _qContract, _mContract, _icContract);
+        hackathonContract = address(supportToken);
+    }
+
+    function deployOnboardingSupportToken(
+        address _user,
+        address _mContract,
+        uint256 _missionId,
+        address _qContract,
+        address payable _icContract
+    ) internal {
+        OnboardingSupportToken supportToken =
+        new OnboardingSupportToken("g0v Onboarding Support Token", "g0vOST", _qContract, _mContract, _missionId, _icContract);
+        onboardingContract = address(supportToken);
+
+        ImpactCurve(_icContract).curve(CurveType.LINEAR, onboardingContract, _user, 0.001 ether, 0, 10, 0, 0, 5, 0);
+    }
+
+    function deployFoodList(address creator) internal {
         // Add first task.
-        taskCreators.push(user);
-        taskDeadlines.push(100000000);
-        taskDetail.push(unicode"台灣零時政府第陸拾次記得投票黑客松");
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"滷肉飯");
+        taskDetail.push(unicode"食材...");
 
         // Add second task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"瀨尿牛肉丸");
+        taskDetail.push(unicode"食材...");
+
+        // Add third task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"臭豆腐");
+        taskDetail.push(unicode"食材...");
+
+        setNewTasksAndMission(
+            taskCreators,
+            taskDeadlines,
+            taskTitles,
+            taskDetail,
+            creator,
+            unicode"臭豆腐大王",
+            unicode"好吃，新奇又好玩！ 食神好棒～"
+        );
+    }
+
+    function deployMeditationJourney(address creator) internal {
+        // Add first task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push("Day 1. Peace flows where mindfulness goes.");
+        taskDetail.push("Guided meditation...");
+
+        // Add second task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskDetail.push("Day 2. Silence is the language of the soul.");
+        taskDetail.push("Guided meditation...");
+
+        // Add third task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push("Day 3. Breathe in calm, breathe out chaos.");
+        taskDetail.push("Guided meditation...");
+
+        // Add fourth task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push("Day 4. Stillness is the key to understanding.");
+        taskDetail.push("Guided meditation...");
+
+        // Add fifth task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push("Day 5. In quietude, truth whispers.");
+        taskDetail.push("Guided meditation...");
+
+        // Add sixth task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push("Day 6. Let go, and let flow in meditation.");
+        taskDetail.push("Guided meditation...");
+
+        // Add seventh task.
+        taskCreators.push(creator);
+        taskDeadlines.push(future);
+        taskTitles.push("Day 7. Rooted in silence, blossoming in peace.");
+        taskDetail.push("Guided meditation...");
+
+        setNewTasksAndMission(
+            taskCreators,
+            taskDeadlines,
+            taskTitles,
+            taskDetail,
+            creator,
+            "Days of Mindful Living",
+            "An onchain journey to infuse daily life with mindfulness practices. Inhale deeply, feeling calmness spread, and exhale slowly, letting all stress melt away. Sink deeper with each breath, letting peace envelop your being."
+        );
+    }
+
+    function deployHackath0n(address user, address user2) internal {
+        // Add first task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"台灣零時政府第伍拾柒次開源普渡黑客松");
+        taskDetail.push(unicode"活動流程...");
+
+        // Add second task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"台灣零時政府第伍拾捌次 11 歲生日快樂黑客松");
+        taskDetail.push(unicode"活動流程...");
+
+        // Add third task.
         taskCreators.push(user2);
-        taskDeadlines.push(1000000000000);
-        taskDetail.push(unicode"台灣零時政府第伍拾玖次輪班寫 code 救台灣黑客松");
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"台灣零時政府第伍拾玖次輪班寫 code 救台灣黑客松");
+        taskDetail.push(unicode"活動流程...");
 
-        // Add third task.
+        // Add fourth task.
         taskCreators.push(user);
-        taskDeadlines.push(1000000000000);
-        taskDetail.push(unicode"台灣零時政府第伍拾捌次 11 歲生日快樂黑客松");
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"台灣零時政府第陸拾次記得投票黑客松");
+        taskDetail.push(unicode"活動流程...");
 
-        // Add third task.
+        // Add fifth task.
         taskCreators.push(user);
-        taskDeadlines.push(1000000000000);
-        taskDetail.push(unicode"台灣零時政府第伍拾柒次開源普渡黑客松");
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"台灣零時政府第陸拾壹次龍來 Open Data Day 黑客松");
+        taskDetail.push(unicode"活動流程...");
 
-        // Submit tasks onchain.
-        Mission(mContract).payToSetTasks(taskCreators, taskDeadlines, taskDetail);
-
-        taskIds.push(1);
-        taskIds.push(2);
-        taskIds.push(3);
-        taskIds.push(4);
-
-        // Submit mission onchain.
-        Mission(mContract).payToSetMission(
+        setNewTasksAndMission(
+            taskCreators,
+            taskDeadlines,
+            taskTitles,
+            taskDetail,
             user,
             unicode"台灣零時政府黑客松",
-            unicode"自台灣發起、多中心化的公民科技社群「零時政府」，以資訊透明、開放成果、開放協作為核心，透過群眾草根的力量來關心公共事務。",
-            taskIds
+            unicode"自台灣發起、多中心化的公民科技社群「零時政府」，以資訊透明、開放成果、開放協作為核心，透過群眾草根的力量來關心公共事務。"
         );
+    }
+
+    function deployHackath0nLunch(address user) internal {
+        // Add first task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"炸雞");
+        taskDetail.push(unicode"食材...");
+
+        // Add second task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"冰紅茶");
+        taskDetail.push(unicode"食材...");
+
+        // Add third task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"熱紅茶");
+        taskDetail.push(unicode"食材...");
+
+        // Add fourth task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"壽司");
+        taskDetail.push(unicode"食材...");
+
+        // Add fifth task.
+        taskCreators.push(user);
+        taskDeadlines.push(future);
+        taskTitles.push(unicode"炒飯");
+        taskDetail.push(unicode"食材...");
+
+        setNewTasksAndMission(
+            taskCreators,
+            taskDeadlines,
+            taskTitles,
+            taskDetail,
+            user,
+            unicode"台灣零時政府當次黑客松之午餐",
+            unicode"自台灣發起、多中心化的公民科技社群「零時政府」，以資訊透明、開放成果、開放協作為核心，透過群眾草根的力量來關心公共事務。"
+        );
+    }
+
+    function _createNewTask(address user, uint256 expiration, string calldata title, string calldata detail) internal {
+        // Clear arrarys.
+        delete taskCreators;
+        delete taskDeadlines;
+        delete taskTitles;
+        delete taskDetail;
+
+        // Add one task.
+        taskCreators.push(user);
+        taskDeadlines.push(expiration);
+        taskTitles.push(title);
+        taskDetail.push(detail);
+
+        // Submit tasks onchain.
+        Mission(mContract).payToSetTasks{value: ImpactCurve(icContract).getCurvePrice(true, 1, 0)}(
+            taskCreators, taskDeadlines, taskTitles, taskDetail
+        );
+    }
+
+    function _createNewTasks(
+        address[] memory users,
+        uint256[] memory expirations,
+        string[] memory titles,
+        string[] memory detail
+    ) internal returns (uint256[] memory) {
+        // Clear arrarys.
+        delete taskCreators;
+        delete taskDeadlines;
+        delete taskTitles;
+        delete taskDetail;
+        delete taskIds;
+
+        // Retrieve task id.
+        uint256 taskId = Mission(mContract).getTaskId();
+
+        if (users.length == expirations.length && expirations.length == detail.length && titles.length == detail.length)
+        {
+            for (uint256 i; i < users.length; i++) {
+                taskCreators.push(users[i]);
+                taskDeadlines.push(expirations[i]);
+                taskTitles.push(titles[i]);
+                taskDetail.push(detail[i]);
+
+                taskIds.push(taskId + i + 1);
+            }
+
+            // Submit tasks onchain.
+            Mission(mContract).payToSetTasks{value: ImpactCurve(icContract).getCurvePrice(true, 1, 0)}(
+                taskCreators, taskDeadlines, taskTitles, taskDetail
+            );
+
+            return taskIds;
+        } else {
+            revert Invalid();
+        }
+    }
+
+    function _createNewMission(address creator, string memory title, string memory detail, uint256[] memory _taskIds)
+        internal
+    {
+        // Submit mission onchain.
+        Mission(mContract).payToSetMission{value: ImpactCurve(icContract).getCurvePrice(true, 1, 0)}(
+            creator, title, detail, _taskIds
+        );
+    }
+
+    function setNewTasksAndMission(
+        address[] memory _taskCreators,
+        uint256[] memory _expirations,
+        string[] memory _taskTitles,
+        string[] memory _taskDetail,
+        address _missionCreator,
+        string memory _title,
+        string memory _missionDetail
+    ) internal {
+        taskIds = _createNewTasks(_taskCreators, _expirations, _taskTitles, _taskDetail);
+        _createNewMission(_missionCreator, _title, _missionDetail, taskIds);
     }
 }
